df83f3b6c65c78dc5ee41f70c971adbcf7462cef
JulioFabio <juliofabiodossantosjfs@gmail.com>
2024-09-03 23:19:51 -0300

    new file:   .vscode/c_cpp_properties.json 	new file:   Codigos/Hud.cpp 	new file:   Codigos/Teste.cpp

---
+++ Codigos/Hud.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int choice = 0;
+
+    cout << "---------------------";
+
+    return 0;
+}
---
+++ Codigos/Teste.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+int choice = 0;
+
+// while(choice != NULL){
+
+    cout << "---------------------";
+
+//     switch (choice)
+//     {
+//         case 1:
+//             break;
+
+
+
+
+
+//         }
+//     }
+// }
+    return 0;
+}
